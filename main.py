f5175db430f77f152a7fb46dd24ab202d900f75a
Cyber-Tapok <slava1152@gmail.com>
2021-09-16 11:52:50 +0400

    Laba done

--- main.py
+++ main.py
@@ -1,24 +1,53 @@
 import numpy as np
 
 
-def createMatrix(row, column):
-    # return np.random.randint(0, 2, (row, column))
-    return np.matrix([[1, 1], [1, 2]])
+# Создаем рандомную матрицу, заполненную 0 и 1 по заданному количеству строк и столбцов
+def create_matrix(row, column):
+    return np.random.randint(0, 2, (row, column))
 
 
-def swapRows(matrix, row1, row2):
+# Поменять местами две заданные строчки матрицы
+def swap_rows(matrix, row1, row2):
     matrix[[row1, row2]] = matrix[[row2, row1]]
 
 
-def SumRows(matrix, row, rowsum):
+# Суммирование двух строчек матрицы, сумма сохраняется в строчку row
+def sum_rows(matrix, row, rowsum):
     matrix[row] = np.array([((a + k) % 2) for a, k in zip(matrix[row], matrix[rowsum])])
 
 
+# Количество строк матрицы
+def row_size(matrix):
+    return matrix.shape[0]
+
+
+# Кличество столбцов матрицы
+def column_size(matrix):
+    return matrix.shape[1]
+
+
+# Ступенчатый вид
+def ref(matrix):
+    column = 0  # Первый столбец
+    leadrow = 0  # Ведущая строка
+    while column < column_size(matrix):  # Идём по столбцам матрицы
+        for r in range(leadrow+1, row_size(matrix)):  # Идём от следующей от ведущей строки до последней строки
+            if matrix[r][column] > matrix[leadrow][column]:  # Если элемент больше
+                currentrow = r  # Запоминаем строчку
+                swap_rows(matrix, currentrow, leadrow)  # Меняем строчку, чтобы была новая ведущая
+            if matrix[r][column] == matrix[leadrow][column]:  # Если элементы равны
+                sum_rows(matrix, r, leadrow)  # Суммируем строчки чтобы занулить элементы
+        if np.all(matrix[leadrow:, column] == 0):  # Если все элементы столбца ниже ведуще строки = 0
+            leadrow = leadrow  # Ведущая строка не меняется
+        else:
+            leadrow += 1  # Ведущая строка становится следующей
+        column += 1  # Переходим к следующему столбцу
+    return matrix  # Возвращаем полученную матрицу
+
+
 if __name__ == '__main__':
-    v = createMatrix(2, 2)
-    print(v)
-    swapRows(v, 0, 1)
-    print(v)
-    SumRows(v, 0, 1)
-    # print([(a+k) for a, k in zip(v[1], v[0])])
-    print(v)
+    rand_matrix = create_matrix(5, 10)  # Создаем матрицу, размер пишем в ()
+    print(rand_matrix)
+    step_matrix = ref(rand_matrix)
+    print(step_matrix)
+
